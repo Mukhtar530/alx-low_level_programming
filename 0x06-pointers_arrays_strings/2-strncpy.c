37d54e7123655619523ab4b341a594fb0b3a9b76
mukhtar muhamamd <mukhtarmuhammad012@gmail.com>
2022-09-21 20:13:31 -0700

    hot fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,27 @@
-#include <main.h>
-#include <string.h>
+#include "main.h"
+
 /**
- * _strncpy -> for copying purpose
- * @dest: param1
- * @src: param2
- * @n: param3
- * Return: a string
+ * _strncpy - cpoies n bytes of src to the dest string
+ * @dest: string to copy to
+ * @src: string being copied
+ * @n: largest number of bytes to copy
+ *
+ * Return: address of dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int i;
+
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
 	return (dest);
 }
